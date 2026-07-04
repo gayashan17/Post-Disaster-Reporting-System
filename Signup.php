720d6578dbef687e55e0309271586c7f092eda9b
gayashan17 <charindugayashan00@gmail.com>
2026-07-04 23:45:08 +0530

    signup Update

--- Signup.php
+++ Signup.php
@@ -20,8 +20,8 @@
 
         try
         {
-            $query = "INSERT INTO users (Username, Password, Full_name, Gender, NIC, Email, Phone_Number, Address)
-                      VALUES (?,?,?,?,?,?,?,?)";
+            $query = "INSERT INTO users (Username, Password, Full_name, Gender, NIC, Email, Phone_Number, Address,Role_ID)
+                      VALUES (?,?,?,?,?,?,?,?,3)";
 
                     $stmt = mysqli_prepare($con, $query);
 
